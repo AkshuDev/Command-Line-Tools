52f796b6a40a7c54d938e11df881d6fcd4de0e6e
AkshuDev <148709451+AkshuDev@users.noreply.github.com>
2025-02-12 12:40:14 +0000

    modified:   src/windows/binReader.cpp

--- src/windows/binReader.cpp
+++ src/windows/binReader.cpp
@@ -1,3 +1,30 @@
 #include <iostream>
 #include <windows.h>
+#include <fstream>
+#include <filesystem>
+#include <variant>
 
+std::string read_binary_file(std::string characterSet, std::string path, bool error=true) {
+    ifstream file(path);
+
+    // Check if file is open
+    if (!file.is_open() {
+        std::string error = "File is not opened successfully! CODE: CLT#src#windows#binReader#11";
+        if (error) {
+            std::cerr << error << std::endl;
+        }
+        return error;
+    })
+
+    std::string content;
+    std::string line;
+
+    // Read Each Line
+    while (getline(file, line)) {
+        content += line;
+    }
+
+    file.close();
+
+    return content;
+}
